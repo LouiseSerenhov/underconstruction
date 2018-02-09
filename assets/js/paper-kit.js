93d3a1260ae431ee3f7b079deeffcab5a3655ffe
LouiseSerenhov <louise.serenhov@hotmail.com>
2018-02-09 12:48:56 +0100

    fixedloggo

--- assets/js/paper-kit.js
+++ assets/js/paper-kit.js
@@ -274,20 +274,5 @@ function debounce(func, wait, immediate) {
 	};
 };
 
-$(function() { var logo = $(".whitelogo"); $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    
-        if (scroll >= 490) {
-          if(!logo.hasClass("blacklogo")) {
-            logo.hide();
-            logo.removeClass('whitelogo').addClass("blacklogo").fadeIn( "");
-          }
-        } else {
-          if(!logo.hasClass("whitelogo")) {
-            logo.hide();
-            logo.removeClass("blacklogo").addClass('whitelogo').fadeIn( "");
-          }
-        }
-    
-    });
-    });
+
+   
